815376dc66f9688deae3527e99a44b1b63793572
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-04-03 13:49:32 +0300

    Сделан вызов проверок

---
+++ src/checkers/index.ts
@@ -0,0 +1,20 @@
+import { Model, ModelCtor } from "sequelize";
+import { checkColumns } from "./columns";
+import { checkParanoid } from "./paranoid";
+import { checkTimestamps } from "./timestamps";
+import { checkUnderscore } from "./underscore";
+
+const checkers: ((
+  model: ModelCtor<Model<any, any>>
+) => void | Promise<void>)[] = [
+  checkTimestamps,
+  checkParanoid,
+  checkUnderscore,
+  checkColumns,
+];
+
+export async function checkAll(model: ModelCtor<Model<any, any>>) {
+  for (const checker of checkers) {
+    await checker(model);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import { checkAll } from "./checkers";
 import { connect } from "./libs/sequelize";
 
-connect();
+(async () => {
+  const sequelize = await connect();
+
+  const rawModels = sequelize.models;
+  const models = Object.values(rawModels);
+
+  for (const model of models) {
+    await checkAll(model);
+  }
+})();
