b8f34e451fe9a3df7bbb9ca233cea35783654c65
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-04-04 13:05:32 +0300

    Добавлена поддержка typescript
    Теперь проверяется не билд моделей после tsc, а файлы .ts.
    • Добавлен .npmignore, чтобы не тащить лишние папки. Также это сделано для того, чтобы Sequelize брался не локальный, а из модулей основного проекта. Иначе при запуске будет ошибка, что эти модели не являются экземплярами нашего класса Model. А они и не являются, потому что это разные Model.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 #! /usr/bin/env node
 import { checkAll } from "./checkers";
 import { connect } from "./libs/sequelize";
+import { register } from "ts-node";
 
 (async () => {
+  register();
+
   const sequelize = await connect();
 
   const rawModels = sequelize.models;
